ec7d8043a5325af30ad024801694a763f661860c
Yure Barbosa Couto <49762865+yurecouto@users.noreply.github.com>
2022-09-24 17:12:45 -0300

    vertical scalling

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
-import 'dotenv/config'
+import "dotenv/config"
 import express from "express";
 import logger from "./utils/logger";
+import cluster, { Worker } from "cluster";
+import os from "os";
 
 import { router } from "./routes/index.routes";
 import connect from "./utils/connect";
@@ -10,10 +12,26 @@ const port = process.env.PORT;
 const app = express();
 
 app.use(express.json());
-
 app.use(router);
 
-app.listen(port, async () => {
+if (cluster.isPrimary) {
+  const cores = os.cpus().length;
+
+  logger.info(`Total cores: ${cores}`);
+  logger.info(`Primary process ${process.pid} is running`);
+
+  for (let i = 0; i < cores; i += 1) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker: Worker, code) => {
+    logger.info(`Worker ${worker.process.pid} exited with code ${code}`);
+    logger.info('Fork new worker!');
+    cluster.fork();
+  });
+} else {
+  app.listen(port, async () => {
     logger.info(`API started at: http://localhost:${port}/`);
     connect()
-});
+  });
+}
